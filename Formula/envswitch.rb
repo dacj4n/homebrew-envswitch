9298aa3d2cfc1723f202e52b705ba950518acaee
dacj4n <irockyou2023@126.com>
2026-05-18 17:52:30 +0800

    change version sha256

--- Formula/envswitch.rb
+++ Formula/envswitch.rb
@@ -7,7 +7,7 @@ class Envswitch < Formula
   on_macos do
     on_arm do
       url "https://github.com/dacj4n/envSwitch/releases/download/v#{version}/envswitch-macos-arm64.tar.gz"
-      sha256 "6e648d6765862e466481a8ca19dc791a435e6d3b9af0a874cdfd2fb797d93da7"
+      sha256 "d43e89aab6270fbf8fa39e5ba50d19c7c8bb71b354096df439faff37b37e4466"
     end
   end
 
@@ -18,7 +18,7 @@ class Envswitch < Formula
   def caveats
     <<~EOS
       Add envSwitch shell integration:
-        envswitch init zsh && source ~/.zshrc
+        envswitch init zsh && source ~/.envswitch/init.sh
     EOS
   end
 
