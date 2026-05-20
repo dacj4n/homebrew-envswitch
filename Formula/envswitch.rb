1548307c10c88d0bc265951265070c95a7a26526
dacj4n <irockyou2023@126.com>
2026-05-20 10:50:17 +0800

    change version sha256 & version to 0.2.1

--- Formula/envswitch.rb
+++ Formula/envswitch.rb
@@ -2,12 +2,12 @@ class Envswitch < Formula
   desc "Native dev-environment version manager — instant symlink switching for JDK, Go, Node, PHP, Python, MySQL, PostgreSQL"
   homepage "https://github.com/dacj4n/envSwitch"
   license "MIT"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     on_arm do
       url "https://github.com/dacj4n/envSwitch/releases/download/v#{version}/envswitch-macos-arm64.tar.gz"
-      sha256 "368913ef85e8959d55f166e4c971efc92a7aba98b57c88931645e496110e99de"
+      sha256 "4380ff3c26336888c3271a083723ad856a436ff9fe15942956132174c18d4474"
     end
   end
 
